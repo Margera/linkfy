7629df551f5237e746017cf985884a579873f820
Bruno Silva <bruno_bls23@hotmail.com>
2023-04-04 22:13:08 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# linkfy
+ API connect Spotify
---
+++ src/main/java/bsoft/com/linkfy/LinkfyApplication.java
@@ -0,0 +1,13 @@
+package bsoft.com.linkfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkfyApplication.class, args);
+	}
+
+}
